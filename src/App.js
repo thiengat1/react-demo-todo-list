7a891714e305af7f844581989fa59e14ebd20dad
thienga <thiengat1@gmail.com>
2020-05-23 00:27:49 -0700

    react demo todo-list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state=
+    {
+      TodoItemArr:[
+        {title:'mua bim bim',isComplete:true},
+        {title:'di da bong',isComplete:false},
+        {title:'di mua ruou',isComplete:false},
+      ]
+    };
+   
+  }
+  onItemClick(){
+    console.log(item);
+    
+  }
+  render(){
+    const { TodoItemArr } = this.state;
+      return (
+        <div className="App">
+          {
+            TodoItemArr.length>0&&TodoItemArr.map((item,index)=>
+            <TodoItem key={index} item={item} onClick={this.onItemClick}/>
+            )
+          }
+          {TodoItemArr.length===0&&'Nothing here'}
+        </div>
+      );
+  }
+ 
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react'
+import classNames from 'classnames'
+import './TodoItem.css'
+class TodoItem extends Component{
+ 
+    render(){
+        const {item,onClick}=this.props;
+        return (
+            <div onClick={onClick} className={classNames('TodoItem',{
+                'TodoItem-complete':item.isComplete
+            })}>
+            <p>{this.props.item.title}</p>
+        </div>
+        );
+         
+    }
+}
+export default TodoItem;
